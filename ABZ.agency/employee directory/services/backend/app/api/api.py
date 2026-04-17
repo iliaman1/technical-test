20bece365ccb92a759c80e47b86686bd26800ea4
ilia <iliaforwork1993@gmail.com>
2026-04-17 15:37:04 +0300

    feat(frontend): Implement employee tree visualization

--- ABZ.agency/employee directory/services/backend/app/api/api.py
+++ ABZ.agency/employee directory/services/backend/app/api/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from .endpoints import employees
+from .endpoints import employees, pages
 
 api_router = APIRouter()
 api_router.include_router(employees.router, prefix="/employees", tags=["employees"])
+api_router.include_router(pages.router, tags=["Pages"])
---
+++ ABZ.agency/employee directory/services/backend/app/api/endpoints/pages.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def get_employee_tree_page(request: Request):
+    return templates.TemplateResponse(request, "index.html")
